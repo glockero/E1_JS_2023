e8335e5a4a62ee38dc6b77e53f143d67b92ae674
Gustavo Galarza <galarzagus@outlook.com>
2023-04-12 11:44:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -102,8 +102,7 @@ ejercicio6(array6);
 //EJERCICIO 7 (Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".)
 console.log("EJERCICO 7");
 let array7 = [1,2,3,4,5,6,7,8,9,10]
-let posicion_a_eliminar = 4 
-array7.splice(posicion_a_eliminar, 1);
+array7.splice(4, 1);
 
 function ejercicio7(num) {
     
